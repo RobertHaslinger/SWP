bf03609afcf1f4819f6aafb82bf2df39c965ae73
Robert Haslinger <robert.haslinger1999@gmail.com>
2021-03-03 17:54:02 +0100

    add itemregistry

---
+++ Prototype/ItemRegistry.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Prototype.Bows;
+using Prototype.Swords;
+
+namespace Prototype
+{
+    public sealed class ItemRegistry
+    {
+
+        private static readonly object _objLock = new object();
+        private static ItemRegistry _instance = null;
+        public static ItemRegistry Instance
+        {
+            get
+            {
+                lock (_objLock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new ItemRegistry();
+                    }
+                    return _instance;
+                }
+            }
+        }
+
+        private Dictionary<string, SwordPrototype> _swords = new Dictionary<string, SwordPrototype>();
+        private Dictionary<string, BowPrototype> _bows = new Dictionary<string, BowPrototype>();
+
+        public SwordPrototype GetSword(string key)
+        {
+            return _swords.TryGetValue(key, out var sword) ? sword : null;
+        }
+
+        public BowPrototype GetBow(string key)
+        {
+            return _bows.TryGetValue(key, out var bow) ? bow : null;
+        }
+
+        public void AddSword(string key, SwordPrototype sword)
+        {
+            _swords.Add(key, sword);
+        }
+
+        public void AddBow(string key, BowPrototype bow)
+        {
+            _bows.Add(key, bow);
+        }
+    }
+}
--- Prototype/Program.cs
+++ Prototype/Program.cs
@@ -6,7 +6,9 @@ namespace Prototype
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Initialize with values
+
+
         }
     }
 }
